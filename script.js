010551e6b63f4466d49e09d7a8a6f08234958f53
Rutvik Patel <rutvikpatel3377@gmail.com>
2025-05-14 18:13:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-let x = 0;
-let y = 0;
- 
-function rotateCube() {
-  x += 1;
-  y += 1;
-  document.getElementById('cube').style.transform = `rotateX(${x}deg) rotateY(${y}deg)`;
-  requestAnimationFrame(rotateCube);
+function getStarted() {
+  alert("Welcome! Jenkins Auto is ready to deploy your pipelines 🚀");
 }
- 
-rotateCube();
